4526bd016a73c9cc12be205a9c6caf944c3ca5ea
Archer Chang <archer.chang32@gmail.com>
2023-09-24 17:34:28 +1000

    add time command

---
+++ Modules/Commands/TimeCommand.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Interactions;
+using HumansCompanion.Services;
+
+namespace HumansCompanion.Modules.Commands
+{
+    public class TimeCommand : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private CommandHandler _handler;
+        private List<TimeZoneInfo> _timeZones;
+
+        public TimeCommand(CommandHandler handler)
+        {
+            _handler = handler;
+            _timeZones = new List<TimeZoneInfo>
+            {
+                TimeZoneInfo.Local
+            };
+        }
+
+        [SlashCommand("time", "Display a list of time in different timezones.")]
+        public async Task Time()
+        {
+            await RespondAsync($"{TimeZoneInfo.Local} {DateTime.Now}", ephemeral: true);
+        }
+    }
+}
