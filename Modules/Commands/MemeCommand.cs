9ec7be786d4531f26ae37bb3ff884537861c25a1
Archer Chang <archer.chang32@gmail.com>
2024-11-05 10:30:43 +1100

    add addmeme command

--- Modules/Commands/MemeCommand.cs
+++ Modules/Commands/MemeCommand.cs
@@ -1,3 +1,4 @@
+using Discord;
 using Discord.Interactions;
 using HumansCompanion.Services;
 
@@ -7,12 +8,17 @@ namespace HumansCompanion.Modules.Commands
     {
         public InteractionService Commands { get; set; }
         private readonly CommandHandler _handler;
-        private readonly Dictionary<string, string> cachedNameURLpairs;
+        private readonly Dictionary<string, string> cachedNameURLpairs = new();
 
-        public MemeCommand(CommandHandler handler, Dictionary<string, string> cachedNameURLpairs)
+        public MemeCommand(CommandHandler handler)
         {
             _handler = handler;
-            this.cachedNameURLpairs = cachedNameURLpairs;
+        }
+
+        [SlashCommand("addmeme", "Add a new meme. Use `file:` or `url:` to upload image.")]
+        public async Task AddMeme(string name, IAttachment? attachment = null, string url = "")
+        {
+            await RespondAsync("Meme added.", ephemeral: true);
         }
     }
 }
